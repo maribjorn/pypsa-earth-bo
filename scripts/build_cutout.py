e4280511f98c3e7fdbc86cb014d60ceba2458337
maribjorn <115071899+maribjorn@users.noreply.github.com>
2023-10-03 13:17:41 +0200

    First successful run

--- scripts/build_cutout.py
+++ scripts/build_cutout.py
@@ -139,16 +139,3 @@ if __name__ == "__main__":
  
  
 
-import cdsapi
-c = cdsapi.Client()
-c.retrieve("southamerica-2013-era5",
-{
-"variable": "temperature",
-"pressure_level": "1000",
-"product_type": "reanalysis",
-"year": "2013",
-"month": "01",
-"day": "01",
-"time": "12:00",
-"format": "grib"
-}, "download.grib")
